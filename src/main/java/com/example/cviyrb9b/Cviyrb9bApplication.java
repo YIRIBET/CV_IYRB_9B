e3c493d9279099badc370fb04c0150f0b3fbc84d
YIRIBET <20223tn077@utez.edu.mx>
2025-05-29 13:48:09 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/com/example/cviyrb9b/Cviyrb9bApplication.java
@@ -0,0 +1,13 @@
+package com.example.cviyrb9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cviyrb9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cviyrb9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cviyrb9b/Cviyrb9bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cviyrb9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cviyrb9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
